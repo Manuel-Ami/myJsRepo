f641b318e263fb7526fb6ef3da96da61255cc3ad
Manuel-Ami <amissahemma241@gmail.com>
2025-09-21 21:40:28 -0900

    Update assignment-6.js

--- assignment-6.js
+++ assignment-6.js
@@ -4,7 +4,7 @@ let n = arr.length;
 for(let i=0;i<n-1;i++){
     for(let j= 0;j<n-1;j++){
 
-if(numArray[j]<arr[j+1]){
+if(arr[j]<arr[j+1]){
     let temp = arr[j+1];
     arr[j+1] = arr[j]
     arr[j]= temp;
