09b25eebdaf6dadeeb29ebdb0918ec4f99871a1d
Manuel-Ami <amissahemma241@gmail.com>
2026-01-05 00:52:03 -0900

    Introducing superbase

---
+++ models/PhotoVault_model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PhotoVaultSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    photoUrl: {
+        type: String,   
+        required: true
+    },
+    description: {
+        type: String,
+        default: 'A photo in the vault',
+    },
+    uploadedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const PhotoVault = mongoose.model("PhotoVault", PhotoVaultSchema);
+module.exports = PhotoVault;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const app = require("./app")
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const createClient = require("@superbase/supabase-js")
+const fs = require("fs");
+
+
 
 dotenv.config({path:'./.env'});
 
-
+const superbaseUrl = process.env.SUPABASE_URL
+const superbaseKey = process.env.SUPABASE_KEY;
 const PORT = process.env.PORT || 5000;
+
+const supabase = createClient(superbaseUrl, superbaseKey)
+
 
 
 app.listen(PORT, () => {
