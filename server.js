2de34fae1d24ac7172f91c4b7c918ce3b2fb5869
Manuel-Ami <amissahemma241@gmail.com>
2026-01-12 19:40:27 -0900

    Addition of new features

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server Is Running On Port ${PORT}`);
+    console.log(`Server Is Running On Port: ${PORT}`);
 })
 
